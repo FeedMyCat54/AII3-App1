5d7127d4bd4d97eacd8655763269950fadf0c396
feedmycat <konstantangelou@gmail.com>
2020-06-08 16:58:12 +0300

    Add location listener

--- app/src/main/java/com/feedmycat/aii3_app1/MainActivity.java
+++ app/src/main/java/com/feedmycat/aii3_app1/MainActivity.java
@@ -1,15 +1,28 @@
 package com.feedmycat.aii3_app1;
 
+import android.Manifest.permission;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+  private List<Marker> markers = new ArrayList<>();
+
   GoogleMap map;
 
   @Override
@@ -29,5 +42,69 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     LatLng Ampelokipoi = new LatLng(40.6454036, 22.9289857);
     map.addMarker(new MarkerOptions().position(Ampelokipoi).title("Ampleokipoi"));
     map.animateCamera(CameraUpdateFactory.newLatLng(Ampelokipoi));
+
+    // The minimum time (in miliseconds) the system will wait until checking if the location changed
+    int minTime = 1000;
+    // The minimum distance (in meters) traveled until you will be notified
+    float minDistance = 5;
+
+    MyLocationListener myLocListener = new MyLocationListener();
+
+    LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+    // Get the criteria you would like to use
+    Criteria criteria = new Criteria();
+    criteria.setPowerRequirement(Criteria.POWER_LOW);
+    criteria.setAccuracy(Criteria.ACCURACY_FINE);
+    criteria.setAltitudeRequired(false);
+    criteria.setBearingRequired(false);
+    criteria.setCostAllowed(true);
+    criteria.setSpeedRequired(false);
+    // Get the best provider from the criteria specified, and false to say it can turn the provider on if it isn't already
+    String bestProvider = locationManager.getBestProvider(criteria, false);
+
+    // Request location updates
+    if (ActivityCompat.checkSelfPermission(this, permission.ACCESS_FINE_LOCATION)
+        != PackageManager.PERMISSION_GRANTED
+        && ActivityCompat.checkSelfPermission(this, permission.ACCESS_COARSE_LOCATION)
+        != PackageManager.PERMISSION_GRANTED) {
+      return;
+    }
+    locationManager.requestLocationUpdates(bestProvider, minTime, minDistance, myLocListener);
+  }
+
+  private class MyLocationListener implements LocationListener {
+
+    @Override
+    public void onLocationChanged(Location loc) {
+      if (loc != null) {
+        // Create the marker for the new location and save it in a list
+        LatLng latLng = new LatLng(loc.getLatitude(), loc.getLongitude());
+        Marker newMarker = map.addMarker(new MarkerOptions().position(latLng).title("title"));
+        map.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        markers.add(newMarker);
+
+        // When there are more than 5 markers remove the oldest one
+        if (markers.size() > 5) {
+          markers.get(0).remove();
+          markers.remove(0);
+        }
+      }
+    }
+
+    @Override
+    public void onProviderDisabled(String arg0) {
+      // Do something here if you would like to know when the provider is disabled by the user
+    }
+
+    @Override
+    public void onProviderEnabled(String arg0) {
+      // Do something here if you would like to know when the provider is enabled by the user
+    }
+
+    @Override
+    public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+      // Do something here if you would like to know when the provider status changes
+    }
   }
 }
