548eb8423116469751a78442c78833dbb49af0e6
feedmycat <konstantangelou@gmail.com>
2020-06-08 21:15:53 +0300

    Add barometer sensor

--- app/src/main/java/com/feedmycat/aii3_app1/BottomSheetDialog.java
+++ app/src/main/java/com/feedmycat/aii3_app1/BottomSheetDialog.java
@@ -2,6 +2,10 @@ package com.feedmycat.aii3_app1;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,15 +15,20 @@ import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
-public class BottomSheetDialog extends BottomSheetDialogFragment implements OnItemSelectedListener {
+public class BottomSheetDialog extends BottomSheetDialogFragment implements OnItemSelectedListener,
+    SensorEventListener {
   private EditText description;
+  private TextView pressureTextView;
   private String selectedColor;
   private BottomSheetListener mListener;
+  private SensorManager sensorManager;
+  private Sensor pressure;
 
   private boolean initialCheck = true;
 
@@ -36,6 +45,7 @@ public class BottomSheetDialog extends BottomSheetDialogFragment implements OnIt
     View v = inflater.inflate(R.layout.bottom_sheet_layout, container, false);
 
     description = v.findViewById(R.id.et_description);
+    pressureTextView = v.findViewById(R.id.tv_pressure);
     // Gets the spinner
     Spinner spinner = v.findViewById(R.id.spinner_color);
     // Creates array adapter with the colors array
@@ -46,6 +56,11 @@ public class BottomSheetDialog extends BottomSheetDialogFragment implements OnIt
     adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
     spinner.setOnItemSelectedListener(this);
     spinner.setAdapter(adapter);
+
+    // Get an instance of the sensor service, and use that to get an instance of
+    // a particular sensor.
+    sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+    pressure = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
 
     return v;
   }
@@ -65,6 +80,32 @@ public class BottomSheetDialog extends BottomSheetDialogFragment implements OnIt
   @Override
   public void onNothingSelected(AdapterView<?> parent) {
 
+  }
+
+  @Override
+  public void onSensorChanged(SensorEvent event) {
+    // Display the pressure in the TextView
+    float millibarsOfPressure = event.values[0];
+    pressureTextView.setText(String.format("%.3f nbar", millibarsOfPressure));
+  }
+
+  @Override
+  public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+  }
+
+  @Override
+  public void onResume() {
+    // Register a listener for the sensor.
+    super.onResume();
+    sensorManager.registerListener(this, pressure, SensorManager.SENSOR_DELAY_NORMAL);
+  }
+
+  @Override
+  public void onPause() {
+    // Unregister the sensor when the activity pauses.
+    super.onPause();
+    sensorManager.unregisterListener(this);
   }
 
   // Interface for handling bottom sheet actions
